b6dfdb0092f14981b25508b387f5f6b7afc48f8b
mattb5 <mattbuffardi@gmail.com>
2016-07-28 19:02:55 -0400

    deploy commit

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,4 +1,6 @@
 import ActiveModelAdapter from 'active-model-adapter';
+import ENV from 'matt-deploy-ember/config/environment';
 
 export default ActiveModelAdapter.extend({
+  host: ENV.apiHost,
 });
--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    apiHost: 'http://localhost:3000/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -40,8 +41,10 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
-  }
+  ENV.baseURL = '/';
+  ENV.locationType = 'hash';
+  ENV.apiHost = 'http://localhost:3000/'
+}
 
   return ENV;
 };
